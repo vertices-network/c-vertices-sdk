b602a2661f4b189b87e14c14d89c03e299abd110
cyril <cyril.fougeray@gmail.com>
2021-04-16 18:54:03 +0200

    Set arch depending on Host arch (fixes issue with MacM1 running from CLI)

--- utils/utils.cmake
+++ utils/utils.cmake
@@ -34,3 +34,20 @@ function(generate_object target suffix type)
             "${CMAKE_CURRENT_BINARY_DIR}/${target}${CMAKE_EXECUTABLE_SUFFIX}" "${CMAKE_CURRENT_BINARY_DIR}/${target}${suffix}"
             )
 endfunction()
+
+
+# In case running on arm64 arch
+if(CMAKE_HOST_SYSTEM_NAME STREQUAL "Darwin")
+    if(CMAKE_HOST_SYSTEM_PROCESSOR STREQUAL "x86_64")
+        # Check whether we are running under Rosetta on arm64 hardware.
+        execute_process(COMMAND sysctl -q hw.optional.arm64
+                OUTPUT_VARIABLE _sysctl_stdout
+                ERROR_VARIABLE _sysctl_stderr
+                RESULT_VARIABLE _sysctl_result
+                )
+        if(_sysctl_result EQUAL 0 AND _sysctl_stdout MATCHES "hw.optional.arm64: 1")
+            set(CMAKE_HOST_SYSTEM_PROCESSOR "arm64")
+        endif()
+    endif()
+    SET(CMAKE_OSX_ARCHITECTURES "${CMAKE_HOST_SYSTEM_PROCESSOR}" CACHE STRING "Build architectures for Mac OS X" FORCE)
+endif()
